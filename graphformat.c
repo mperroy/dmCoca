a50d9ee0481b08c0252f0e1aa114f6eacbe93ccd
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2017-10-17 13:33:26 +0200

    Manipulation clause et variables de clauses

--- graphformat.c
+++ graphformat.c
@@ -2,13 +2,42 @@
 // include biblio pour manipuler file
 
 clause createClause(){
-  return NULL;
-}
-
-void deleteLastClause(clause * cnf){
+  clause cnf = malloc(sizeof(clause));
+  cnf->varList = createVarList();
+  cnf->nbVar = 0;
+  cnf->next = NULL;
+  return cnf;
 }
 
 void addClause(clause * cnf){
+  while(cnf->next != NULL){
+    cnf = cnf->next;
+  }
+  cnf = createClause();
+  cnf->varList = malloc(sizeof(intList*));
+}
+
+void freeClause(clause * cnf){
+  if(cnf->next != NULL)
+    freeClause(cnf->next);
+  freeVarList(cnf->varList);
+  free(cnf);
+}
+
+void addVar(clause * cnf, int var){
+  cnf->nbVar++;
+  intList * tmp = cnf->varList;
+  while(tmp->next != NULL){
+    tmp = tmp->next;
+  }
+  tmp->next = malloc(sizeof(intList*));
+  tmp->next->var = var;
+}
+
+void freeVarList(intList * varList){
+  if(varList->next != NULL)
+    freeVarList(varList->next);
+  free(varList);
 }
 
 void uniqueIntPerVertice(clause * cnf){
--- graphformat.h
+++ graphformat.h
@@ -3,13 +3,30 @@
 #include "all.h"
 
 struct clause{
-  int * varTab;
+  intList * varList;
   int nbVar;
   clause* next;
 };
 
+struct intList{
+  int var;
+  intList* next;
+};
+
 clause createClause();
 
-void deleteLastClause(clause * cnf);
-
 void addClause(clause * cnf);
+
+void freeClause(clause * cnf);
+
+void addVar(clause * cnf, int var);
+
+void freeVarList(intList * varList);
+
+void uniqueIntPerVertice(clause * cnf);
+
+void uniqueRoot(clause * cnf);
+
+void atLeastOneVerticeAtK(clause * cnf, int k);
+
+void potentialParent(clause * cnf);
