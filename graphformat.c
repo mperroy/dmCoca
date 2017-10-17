5acfd0dfef158debc696e591bccbf215cf99be53
Lucas Ghibert <lghibert@emi.u-bordeaux1.fr>
2017-10-17 13:03:39 +0200

    J'avais raison

---
+++ graphformat.c
@@ -0,0 +1,30 @@
+#include "graphformat.h"
+// include biblio pour manipuler file
+
+clause createClause(){
+  return NULL;
+}
+
+void deleteLastClause(clause * cnf){
+}
+
+void addClause(clause * cnf){
+}
+
+void uniqueIntPerVertice(clause * cnf){
+}
+
+void uniqueRoot(clause * cnf){
+}
+
+void atLeastOneVerticeAtK(clause * cnf, int k){
+}
+
+void potentialParent(clause * cnf){
+}
+
+int main(int argc, char ** args){
+  // check pour k bien reçu
+  // ouverture + écriture fichier cnf retour
+  return 0;
+}
